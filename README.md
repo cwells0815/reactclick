aeea208d604a98706bbf662a7e5e4c7395107247
colin gaming <n.wells3@icloud.com>
2018-10-11 14:07:40 -0400

    reactclick

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# reactclick
+# Clicky Game
+
+## Overview
+This is a simple memory game built in react.
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => (
+  <div className="jumbotron p-3">
+    <h1 className="text-center"></h1>
+    <h3 className="text-center">Click on an image to earn points, dont forget what you have clicked!</h3>
+  </div>
+
+)
+export default Header;
